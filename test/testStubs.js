2e61864d857be0f30901c8766bcc8997d0a2210d
Alex Tzinov <alex.tzinov@gmail.com>
2016-06-14 13:33:40 -0600

    Place to store sample responses to be tested against

---
+++ test/testStubs.js
@@ -0,0 +1,13 @@
+module.exports.samplePredicate = {
+  equals: {
+    method: 'get',
+    path: '/newpage' }
+  }
+
+module.exports.sampleReponse = {
+  is: {
+    statuscode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({"hello" : "world"})
+  }
+}
