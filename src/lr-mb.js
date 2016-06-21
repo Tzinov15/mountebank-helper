fcbd7beeb94f91978122bbbbd72d67afb125ce4e
Alex Tzinov <alex.tzinov@gmail.com>
2016-06-21 11:03:15 -0600

    Added more promise stuff, execution times, error handling, spellin

--- src/lr-mb.js
+++ src/lr-mb.js
@@ -1,7 +1,15 @@
 'use strict';
-// TODO: file streaming for mb
-// TODO: Some kind of promised based wrapper around timing functions in NODE that accoutns for the async behavior of node
-// TODO: ERROR CHECKING
+// IDEA:20 file streaming for mb
+// IDEA:0 Some kind of promised based wrapper around timing functions in NODE that accounts for the async behavior of node
+// DONE:50 ERROR CHECKING
+// TODO:0 Make sure that resolve value is JSON from updateResponse and addRoute
+// TODO:10 Make function that also converts from mountebank form to clean JSON form
+// DOING:0 Cleanup formatting so that it is meeting the linting rules
+// TODO:20 Make sure that the body (in the mb-style body) is formatted correctly (not too many quotes)
+// DONE:0 Ensure that promises are returned from asynchronous methods as opposed to throwing errors
+// DONE:20 Tag all asynchronous methods
+// DONE:10 Time the functions that will be involved as potential bottle necks for large responses
+// DONE:30 Find a way to test multiple (dynamic number) of update calls for the purposes of timing
 
 
 const fetch = require('node-fetch');
@@ -9,7 +17,9 @@ const _ = require('lodash');
 const mb = require('mountebank');
 const util = require('util');
 
+/* EXECUTION TIME: 6-7 seconds for adding 400,000 random routes, parsing into mountebank-style body, and posting entire imposter to Mountebank */
 class Imposter {
+
   /**
   * Sets up the skelton for the.routeInformation POST request body that will be sent to the Mountebank server to set up the imposter]
   * @param  {Number} port     The port.routeInformation number that the imposter is to listen on for incoming requests
@@ -31,9 +41,9 @@ class Imposter {
     };
   }
 
-  /**
+  /** EXECUTION TIME: 132 ms for 400,000 random routes (100,000 random URIs + 4 verbs) being added via addRoute()
   * [Takes in a route (URI + VERB) and a response body that is to be returned from MB when the given route gets reached]
-  * @param  {Object} routeOptions     The options contianing information on the route + corresponding mocked respone
+  * @param  {Object} routeOptions     The options containing information on the route + corresponding mocked response
   * @param  {String} routeOptions.uri The URI of the route the user is wanting to match against
   * @param  {String} routeOptions.verb       The HTTP verb the is wanting to match against
   * @param  {Object} routeOptions.res The desired response that is to be returned when the above URI and method get matched
@@ -67,7 +77,7 @@ class Imposter {
     }
 
     // If the user doesn't provide a path with a leading slash, we will add it here
-    if (routeOptions.uri[0] !== '/') routeOptions.uri = '/' + routeOptions.uri;
+    if (routeOptions.uri[0] !== '/') routeOptions.uri = `/${routeOptions.uri}`;
 
     /* If we already have an existing object for the given URI (from a different verb),
     * we just want to add the new key value pair consisting of our new verb and its respective response */
@@ -82,7 +92,7 @@ class Imposter {
     }
   }
 
-  /**
+  /** EXECUTION TIME: 85 ms for 400,000 random routes (100,000 random URIs + 4 verbs) being parsed from our state and added to our MB-body
   * This will take our state (our swagger-like representation of our routes) and construct our mountebank-formatted body
   * This mountebank-formatted body is what gets inserted into our POST request which ultimately creates our imposter
   *  @returns {Object} - A rigidly formatted MounteBank-Friendly object that can be directly sent as a post request to MB
@@ -98,7 +108,7 @@ class Imposter {
     for (const route in this.ImposterInformation.routeInformation) {
       // for each verb contained within that one route...
       for (const verb in this.ImposterInformation.routeInformation[route]) {
-        // extract the necassary attributes from our response (a verb and route uniquely identifies a single response)
+        // extract the necessary attributes from our response (a verb and route uniquely identifies a single response)
         const statusCode = this.ImposterInformation.routeInformation[route][verb].statusCode;
         const responseHeaders = this.ImposterInformation.routeInformation[route][verb].responseHeaders;
         const responseBody = this.ImposterInformation.routeInformation[route][verb].responseBody;
@@ -107,7 +117,7 @@ class Imposter {
         const mbResponse = Imposter._createResponse(statusCode, responseHeaders, responseBody);
         const mbPredicate = Imposter._createPredicate('equals', { 'method' : verb, 'path' : route } );
 
-        // shove these portions into our final complete response
+        // shove these portions into our final complete response in the form of a stub
         CompleteResponse.stubs.push({
           predicates:[mbPredicate],
           responses: [mbResponse]
@@ -149,7 +159,7 @@ class Imposter {
   * This will take in the users desired predicate components and construct a mounte-bank style predicate
   * @param  {String} operator   The operator to be used as part of this predicate (see mountebank predicate documentation for list of available operators)
   * @param  {Object} predicateBody       The body of the predicate. Often contains information on what conditions are to be met for a match on incoming request
-  * @return {Object}            The mountebank-formatted prediate object that can be added as part of a mountebank stub
+  * @return {Object}            The mountebank-formatted predicate object that can be added as part of a mountebank stub
   */
   static _createPredicate(operator, predicateBody) {
     if (!_.isString(operator)) {
@@ -164,11 +174,11 @@ class Imposter {
   }
 
   /**
-  * Helper function that will retreive a response from our swagger-like state based on the supplied path information (verb + uri)
-  * @param  {Object} pathToUpdate     An object containting information on which path we're retreiving a response for
+  * Helper function that will retrieve a response from our swagger-like state based on the supplied path information (verb + uri)
+  * @param  {Object} pathToUpdate     An object containing information on which path we're retrieving a response for
   * @param  {String} pathToUpdate.verb     The HTTP method for the complete path
   * @param  {String} pathToUpdate.uri     The relative URI for the complete path the user wants to update
-  * @return {Object} The response object (contains statusCode, headers, body) or throws an error if it can't find it
+  * @return {Object} The response object (contains statusCode, headers, body) or null if it can't find it
   */
   _getResponse(pathToUpdate) {
     /* Input Validation */
@@ -183,35 +193,56 @@ class Imposter {
     }
     const verb = pathToUpdate.verb;
     const uri =  pathToUpdate.uri;
-    const responseToUpdate = this.ImposterInformation.routeInformation[uri][verb];
-    if (responseToUpdate == null) throw new ReferenceError(`Could not find a response for ${verb} ${uri}`);
+    var responseToUpdate;
+
+    // try to retrieve the response from our state based on the passed in uri and verb
+    try {
+      responseToUpdate = this.ImposterInformation.routeInformation[uri][verb];
+    }
+    catch (e) {
+      if (e instanceof TypeError) { // A TypeError will be thrown if the uri supplied doesn't exist in our swagger-like state
+        console.error(`ERROR (_getResponse) : Could not find a response for ${verb} ${uri}`);
+        throw new TypeError(e.message);
+      }
+      else {
+        console.error(`ERROR: ${e.message}`);
+        throw new TypeError(e.message);
+      }
+    }
+    // if the uri exists but the corresponding supplied verb does NOT, a TypeError above won't be thrown, but
+    // responseToUpdate will be null so we check for that here
+    if (responseToUpdate == null) {
+      console.error(`ERROR: Could not find a response for ${verb} ${uri}`);
+      throw new TypeError(`ERROR: Could not find a response for ${verb} ${uri}`);
+    }
+    // Return our successfully retrieved response
     return responseToUpdate;
   }
 
-  /**
+  /** ASYNC-METHOD
    * deletes the old imposter
-   * mountebank will return details on the deleted imposter upon a succesful delete request
+   * mountebank will return details on the deleted imposter upon a successful delete request
    * unfortunately, it will also return a 200 even when attempting to delete a non-existing imposter
-   * therefore, to validate a succesful delete request (for the purpose of maintaining resolved promises),
+   * therefore, to validate a successful delete request (for the purpose of maintaining resolved promises),
    * we check the body of the response from the delete request
-   * @return {Promise}   If we have a succesful delete request (see above) then we return a resolved promise containting the contents of the deleted imposter
+   * @return {Promise}   If we have a successful delete request (see above) then we return a resolved promise containing the contents of the deleted imposter
    */
   _deleteOldImposter() {
+    // make DELETE request to the mountebank server (through fetch)...
     return fetch(`http://127.0.0.1:2525/imposters/${this.ImposterInformation.port}`, { method: 'delete' })
-    .then(function (response) {
+    .then(function (response) {   // retrieve the text body from the response
       return response.text();
     })
     .then(function (body) {
-      if (body === '{}' ) {
-        throw new Error('old imposter was never deleted');
+      if (body === '{}' ) { // Return rejected promise in case of imposter that never got deleted (or was never there to begin with)
+        return Promise.reject(new Error('old imposter was never deleted'));
       }
       else {
-        return body;
+        return Promise.resolve(body); // Return resolved promise containing text body from response
       }
     })
     .catch(function (error) {
-      console.log(error);
-      throw new Error('old imposter was never deleted');
+      return Promise.reject(error); // Return rejected promise for any other issue
     });
   }
 
@@ -223,6 +254,7 @@ class Imposter {
   * @param  {String} pathToUpdate.uri     The relative URI for the complete path the user wants to update
   * @returns {Promise} Will return the promise returned by _updateResponse which will be resolved with the response from the fetch call
   **/
+
   updateResponseCode(newCode, pathToUpdate) {
     /* Input Validation */
     if (!_.isObject(pathToUpdate)) {
@@ -249,6 +281,7 @@ class Imposter {
   * @param  {String} pathToUpdate.uri     The relative URI for the complete path the user wants to update
   * @returns {Promise} Will return the promise returned by _updateResponse which will be resolved with the response from the fetch call
   **/
+
   updateResponseHeaders(newHeaders, pathToUpdate) {
     /* Input Validation */
     if (!_.isObject(pathToUpdate)) {
@@ -273,6 +306,7 @@ class Imposter {
   * @param  {String} pathToUpdate.uri     The relative URI for the complete path the user wants to update
   * @returns {Promise} Will return the promise returned by _updateResponse which will be resolved with the response from the fetch call
   **/
+
   updateResponseBody(newBody, pathToUpdate) {
     /* Input Validation */
     if (!_.isObject(pathToUpdate)) {
@@ -290,10 +324,12 @@ class Imposter {
     return this._updateResponse(newBody, 'responseBody', pathToUpdate);
   }
 
-  /**
+  /** ASYNC-METHOD
+  * EXECUTION TIME: ~700 ms for generating two random words, updating an imposter body with the two words, and posting to MB 100 times
+  * (100 DELETE -> POST requests)
   * This method is being overloaded in order to simplify and consolidate repeating logic
   * The methods updateResponseBody, updateResponseHeaders, updateResponseCode will all call this method with the respective parameters
-  * The type of newContent will change depending on the funciton that calls _updateResponse, and the value of attributeToUpdate will respecitvely change
+  * The type of newContent will change depending on the funciton that calls _updateResponse, and the value of attributeToUpdate will respecitvely change as well
   * @param  {Number/String/Body} newContent   The new content that the user wants to set for the specified path
   * @param  {String} attributeToUpdate   The specific attribute within the response that is being updated. This will be specified based on which function calls this
   * @param  {Object} pathToUpdate     An object containting information on which path they wish to update
@@ -314,6 +350,10 @@ class Imposter {
     if (!_.isString(attributeToUpdate)) {
       throw new TypeError('attributeToUpdate must be a string');
     }
+
+    // If the user doesn't provide a path with a leading slash, we will add it here
+    if (pathToUpdate.uri[0] !== '/') pathToUpdate.uri = `/${pathToUpdate.uri}`;
+
     // Get the response we are looking to modify
     const responseToUpdate = this._getResponse(pathToUpdate);
 
@@ -327,12 +367,14 @@ class Imposter {
     return this._deleteOldImposter().then(function () {
       return fetch('http://127.0.0.1:2525/imposters', { method: 'post', headers: { 'content-type' : 'application/json' }, body: JSON.stringify(updatedMounteBankRequest) });
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch(function (error) { // this will catch errors (rejected promises) from the _deleteOldImposter method as well as from the above fetch call to update
+      return Promise.reject(error);
     });
   }
 
+
   /** CLIENT-FACING METHOD
+  * ASYNC METHOD
   * This will take the current Imposter object (this) and make the POST request to the mountebank server to create the new imposter
   * @return {Object}           Returns a promise (returns the node-fetch promise) that resolves the response and rejects with the error message
   */
@@ -348,7 +390,7 @@ class Imposter {
   }
   printCompleteResponse() {
     console.log('~~~  Complte Response (Mountebank-Like structure)~~~~');
-    console.log(util.inspect(this.CompleteResponse, { depth: null }));
+    console.log(util.inspect(this._createMBPostRequestBody(), { depth: null }));
   }
 }
 
